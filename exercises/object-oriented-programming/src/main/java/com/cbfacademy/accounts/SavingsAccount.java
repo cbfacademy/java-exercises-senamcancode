eb5678a9a4071f36085f6e3491302c07c27317c2
Senam <senamdaniel@gmail.com>
2023-08-03 17:58:42 +0100

    added OOP account practice files

---
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/accounts/SavingsAccount.java
@@ -0,0 +1,16 @@
+package com.cbfacademy.accounts;
+
+public class SavingsAccount extends Account {
+    final static private double INTEREST_PERCENTAGE = 6.7;
+
+    public SavingsAccount(double balance, int accountNumber) {
+        super(balance, accountNumber);
+    }
+
+    public void addInterest() {
+        double interestAmount = this.balance * (INTEREST_PERCENTAGE / 100);
+        this.balance = this.balance + interestAmount;
+
+    }
+
+}
--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/dogs/Main.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/dogs/Main.java
@@ -5,16 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         Dog dogOne = new Dog("poodle", 'L', 4, "black");
-        Dog dogTwo = new Dog("lab", 'M', 5, "white"); 
-        Dog dogThree = new Dog ("bulldog", 'M', 2, "brown");
-
+        Dog dogTwo = new Dog("lab", 'M', 5, "white");
+        Dog dogThree = new Dog("bulldog", 'M', 2, "brown");
 
         dogOne.run();
 
         System.out.println(dogTwo.getAge());
 
         System.out.println(dogThree.getBreed());
-
 
     }
 
