0513bda28ab1393f6b27bcc55aad03fe9482711d
Senam <senamdaniel@gmail.com>
2023-08-21 20:34:47 +0100

    completed the input/output exercises

--- exercises/input-output/src/main/java/com/cbfacademy/App.java
+++ exercises/input-output/src/main/java/com/cbfacademy/App.java
@@ -1,13 +1,57 @@
 package com.cbfacademy;
 
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.*; 
+
+
 public class App {
+
     public static void fileReaderMethod() {
-        String test = "";
-       
+        String fileName = "/Users/senam/Documents/Code/entry-to-tech/java-exercises-senamcancode/exercises/input-output/src/main/resources/exercise.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.print(line);
+                
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("File reading exception: " + e.getMessage());
+
+        }
     }
+
+    public static void fileWriterMethod(){
+        String writerFile = "/Users/senam/Documents/Code/entry-to-tech/java-exercises-senamcancode/exercises/input-output/src/main/resources/exercise2.txt";
+        String fileName = "/Users/senam/Documents/Code/entry-to-tech/java-exercises-senamcancode/exercises/input-output/src/main/resources/exercise.txt";
+    
+        try (
+        FileOutputStream fileOutputStream = new FileOutputStream(writerFile); 
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream); 
+        PrintWriter writer = new PrintWriter(outputStreamWriter); 
+
+        
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.printf(line); 
+            
+            }
+            outputStreamWriter.close(); 
+        }  catch (IOException e) {
+            System.out.println("File writing exception: " + e.getMessage());
+
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        fileReaderMethod();
+        fileWriterMethod(); 
 
     }
 }
