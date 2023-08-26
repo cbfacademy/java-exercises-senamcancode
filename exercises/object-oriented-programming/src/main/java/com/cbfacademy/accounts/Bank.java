44ea778705c4fbec9d1ae7b7a625b650b293f91e
Senam <senamdaniel@gmail.com>
2023-08-26 23:06:06 +0100

    completed the bank OOP exercise

--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/accounts/Bank.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/accounts/Bank.java
@@ -4,68 +4,70 @@ import java.util.ArrayList;
 
 public class Bank extends Account {
 
+    public Bank(double balance, int accountNumber) {
+        super(balance, accountNumber);
+        // TODO Auto-generated constructor stub
+    }
+
+    ArrayList<Account> accountsList = new ArrayList<>();
+
     public void main(String[] args) {
-        ArrayList<Account> accountsList = new ArrayList<>();
 
     }
 
     public void openCurrentAccount() {
-        
+        Account currentAccount = new CurrentAccount(balance, getAccountNumber(), balance);
+        accountsList.add(currentAccount);
     }
 
     public void openSavingsAccount() {
-
+        Account savingsAccount = new SavingsAccount(balance, getAccountNumber(), balance);
+        accountsList.add(savingsAccount);
     }
 
     public void closeSavingsAccount() {
+        for (Account account : accountsList) {
+            if (account instanceof SavingsAccount) {
+                accountsList.remove(account);
+            }
+        }
 
     }
 
     public void closeCurrentAccount() {
-
-    }
-
-    public void updateAccount(){
-        ArrayList<Account> accountsList = new ArrayList<>();
-        for (int i = 0; i < accountsList.size(); i++){
-            if(Account instanceof SavingsAccount){
-                getInterest(); 
+        for (Account account : accountsList) {
+            if (account instanceof CurrentAccount) {
+                accountsList.remove(account);
             }
         }
-    
+
     }
 
-    // public void payDividend(){
-    //     double interestEarned = this.balance + getInterest();}
- }
+    public void updateAccount() {
 
+        for (int i = 0; i < accountsList.size(); i++) {
+            Account account = accountsList.get(i);
+            if (account instanceof SavingsAccount) {
+                ((SavingsAccount) account).getInterest();
+            } else if (account instanceof CurrentAccount) {
+                CurrentAccount currentAccount = (CurrentAccount) account; // Cast to CurrentAccount
+                if (currentAccount.getBalance() < 0
+                        && currentAccount.getBalance() > (currentAccount.getOverdraftLimit() * -1)) {
+                    System.out.println("You are in your overdraft, a letter has been sent");
+                }
+            }
+        }
+    }
 
+    public void payDividend() {
+        for (Account account : accountsList) {
+            if (account instanceof SavingsAccount) {
+                SavingsAccount savingsAccount = (SavingsAccount) account;
+                double interestEarned = savingsAccount.balance * savingsAccount.getInterest();
+                double payDividend = interestEarned + savingsAccount.balance;
+                savingsAccount.balance += payDividend;
+            }
 
-// ArrayList<Account> accounts = new ArrayList<>();
-//
-// public void openSavingsAccount() {
-// accounts.add(new SavingsAccount(0,0));
-// }
-//
-// public void openCurrentAccount() {
-// accounts.add(new CurrentAccount(0, 0, 500));
-// }
-//
-// public double getInterest(){
-// return INTEREST_PERCENTAGE;
-// }
-//
-// public void update() {
-//
-// for (int i = 0; i < accounts.size(); i++){
-//
-// if (account instanceOf SavingsAccount) {
-// getInterest()
-// }
-//
-// else if( account at index i == CurrentAccount )
-//
-//
-//
-// }
-// }
+        }
+    }
+}
